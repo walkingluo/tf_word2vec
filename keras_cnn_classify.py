6ed3a3a56b80771eafed83589496018cebc64483
jiangluo <rojoe07@gmail.com>
2017-03-28 01:39:06 +0800

    update keras f1 metrics

--- keras_cnn_classify.py
+++ keras_cnn_classify.py
@@ -4,9 +4,10 @@ from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Conv1D, GlobalMaxPooling1D
 from keras.layers import LSTM
-from keras.layers import Dropout, Dense, Activation
+from keras.layers import Dropout, Dense, Activation, SpatialDropout1D
 from keras.layers import Embedding
 from keras.callbacks import Callback, EarlyStopping
+import keras.backend as K
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -28,14 +29,14 @@ def load_train_data(filename):
     f = open(filename, 'r')
     train = []
     label = []
-    for line in f.readlines()[:200000]:
+    for line in f.readlines()[:1000000]:
         line = line.strip().decode('utf-8').split()
         train.append(line[:-1])
         t_label = int(line[-1])
         if t_label == 2:
-            label.append(1)
+            label.append(1.)
         else:
-            label.append(0)
+            label.append(0.)
     return train, label
 
 
@@ -49,13 +50,15 @@ def load_test_data(filename):
         if t_label == 1:
             continue
         elif t_label == 2:
-            label.append(1)
+            label.append(1.)
         else:
-            label.append(0)
+            label.append(0.)
         test.append(line[:-1])
     return test, label
 
 train, train_label = load_train_data('./weibo/train_set.txt')
+print len(train)
+print len(train_label)
 # test, test_label = load_test_data('./NLPCC/test_data_nlpcc14_weibo.txt')
 
 
@@ -65,7 +68,7 @@ def read_vec(filename):
     embeddings = []
     words = []
     for line in f.readlines():
-        words.append(line.split()[0])
+        words.append(line.split()[0].decode('utf-8'))
         embeddings.append([float(num) for num in line.split()[1:]])
         # embeddings.append(line.split()[1:])
     f.close()
@@ -76,6 +79,7 @@ vocabulary_size = int(vocabulary_size)
 embedding_dim = int(embedding_dim)
 embeddings = np.array(embeddings)
 print len(words)
+print words[:2]
 
 
 def build_dict(words):
@@ -87,14 +91,13 @@ def build_dict(words):
 
 def word_to_id(tweets, dictionary):
     tweets_to_id = []
-    global c
     for tweet in tweets:
         tweet_to_id = []
         for w in tweet:
             try:
                 tweet_to_id.append(dictionary[w])
             except Exception as e:
-                tweet_to_id.append(dictionary['UNK'])
+                tweet_to_id.append(0)
         tweets_to_id.append(tweet_to_id)
     return tweets_to_id
 
@@ -102,6 +105,7 @@ dictionary = build_dict(words)
 train_id = word_to_id(train, dictionary)
 # test_id = word_to_id(test, dictionary)
 print len(train_id)
+print train_id[:2]
 
 '''
 count = 0
@@ -114,8 +118,9 @@ for i in range(vocabulary_size):
         continue
 print count
 '''
-train_num = 100000
-vaild_num = train_num + 50000
+
+train_num = 800000
+vaild_num = train_num + 100000
 max_weibo_length = 140
 
 X_train = train_id[:train_num]
@@ -151,9 +156,10 @@ model = Sequential()
 model.add(Embedding(vocabulary_size,
                     embedding_dim,
                     weights=[embeddings],
-                    trainable=False,
-                    input_length=max_weibo_length,
-                    dropout=0.2))
+                    trainable=True,
+                    input_length=max_weibo_length))
+model.add(SpatialDropout1D(0.2))
+
 model.add(Conv1D(250, 3, padding='valid', activation='relu', strides=1))
 model.add(GlobalMaxPooling1D())
 
@@ -164,12 +170,65 @@ model.add(Activation('relu'))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
+
+def precision(y_true, y_pred):
+    """Precision metric.
+    Only computes a batch-wise average of precision.
+    Computes the precision, a metric for multi-label classification of
+    how many selected items are relevant.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+def recall(y_true, y_pred):
+    """Recall metric.
+    Only computes a batch-wise average of recall.
+    Computes the recall, a metric for multi-label classification of
+    how many relevant items are selected.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+
+def fbeta_score(y_true, y_pred, beta=1):
+    """Computes the F score.
+    The F score is the weighted harmonic mean of precision and recall.
+    Here it is only computed as a batch-wise average, not globally.
+    This is useful for multi-label classification, where input samples can be
+    classified as sets of labels. By only using accuracy (precision) a model
+    would achieve a perfect score by simply assigning every class to every
+    input. In order to avoid this, a metric should penalize incorrect class
+    assignments as well (recall). The F-beta score (ranged from 0.0 to 1.0)
+    computes this, as a weighted mean of the proportion of correct class
+    assignments vs. the proportion of incorrect class assignments.
+    With beta = 1, this is equivalent to a F-measure. With beta < 1, assigning
+    correct classes becomes more important, and with beta > 1 the metric is
+    instead weighted towards penalizing incorrect class assignments.
+    """
+    if beta < 0:
+        raise ValueError('The lowest choosable beta is zero (only precision).')
+
+    # If there are no true positives, fix the F score at 0 like sklearn.
+    if K.sum(K.round(K.clip(y_true, 0, 1))) == 0:
+        return 0
+
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    bb = beta ** 2
+    fbeta_score = (1 + bb) * (p * r) / (bb * p + r + K.epsilon())
+    return fbeta_score
+
 model.compile(loss='binary_crossentropy', optimizer='adam',
-              metrics=['accuracy'])
+              metrics=['accuracy', precision, recall, fbeta_score])
 
 history = LossHistory()
-model.fit(X_train, y_train, validation_data=(X_valid, y_vaild), nb_epoch=2,
-          batch_size=32, callbacks=[history])
+model.fit(X_train, y_train, validation_data=(X_valid, y_vaild), epochs=1,
+          batch_size=128, callbacks=[history])
 
 score = model.evaluate(X_test, y_test)
 
@@ -177,7 +236,6 @@ print model.metrics_names
 print score
 print "Test loss: ", score[0]
 print "Test accuracy: ", score[1]
-print len(history.losses)
 
 '''
 print history.history.keys()
